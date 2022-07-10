005335e6661bb31746a6b26f6e59044e0a6d60fd
CarvalhoCoutoBruno <bruno.carvalho@gmail.com>
2022-07-10 20:05:35 -0300

    first commit

---
+++ src/main/java/com/texo/filmproducer/FilmProducerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.texo.filmproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmProducerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmProducerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texo/filmproducer/FilmProducerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texo.filmproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmProducerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
